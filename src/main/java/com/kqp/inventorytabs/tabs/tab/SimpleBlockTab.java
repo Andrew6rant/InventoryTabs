38d1b97873a6c926db992ace8459244cc0cfc12e
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2022-08-08 16:18:19 +1200

    Update SimpleBlockTab.java

--- src/main/java/com/kqp/inventorytabs/tabs/tab/SimpleBlockTab.java
+++ src/main/java/com/kqp/inventorytabs/tabs/tab/SimpleBlockTab.java
@@ -87,7 +87,7 @@ public class SimpleBlockTab extends Tab {
 
         if (blockEntity != null) {
             NbtCompound tag = new NbtCompound();
-            // blockEntity.writeNbt(tag); // temporarily removing this because writeNBT was changed to protected
+            blockEntity.writeNbt(tag);
 
             if (tag.contains("CustomName", 8)) {
                 return Text.Serializer.fromJson(tag.getString("CustomName"));
